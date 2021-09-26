1ffb10e0c8dfccef1c42ffdba3640402158dde6a
Ramasanjaya22 <ramasanjaya2202@gmail.com>
2021-09-26 11:51:44 +0700

    HelloWorld.java

---
+++ praktikum 2/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+	
+	public static void main(String[] args){
+		System.out.println("Name: Rama Sanjaya");
+		System.out.println("Nim:6706213021");
+		System.out.println("Kelas: 45-04");
+	}
+}
